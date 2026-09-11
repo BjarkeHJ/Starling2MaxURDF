50fe86cc43ad0967172631e279614d26ae155c50
BjarkeHJ <bjarke_holm@hotmail.com>
2026-09-11 15:42:46 +0200

    ..

--- model/make_urdf.py
+++ model/make_urdf.py
@@ -1,42 +1,70 @@
 
 import os
-import argparse
 import jinja2
+import math
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-MM_TO_M = 0.001
 
+MM_TO_M = 0.001
 def scale_mm(xyz: list[float]) -> str:
     return " ".join(str(v * MM_TO_M) for v in xyz)
 
+# Datasheet
+MASS = 0.566
+MASS_W_PAYLOAD = 0.930 
+NROT = 4
+KV = 1500
+VBAT_MAX = 16.8
+VBAT_NOM = 14.8
+T2W = 2.0
+PROP_RADISU = 0.09 # [m]
+RPM_MAX = KV * VBAT_MAX * 0.85
+
 # Measured Quantities
+# Drone BBOX
 SIZE_X = 217 * MM_TO_M
 SIZE_Y = 287 * MM_TO_M
 SIZE_Z = 129 * MM_TO_M
 
+# Motor shafts relative to mesh origin
 PROP0 = scale_mm([94.96, 129.91, 24.06])
 PROP1 = scale_mm([94.96, -129.91, 24.06])
 PROP2 = scale_mm([-94.96, -129.91, 24.06])
 PROP3 = scale_mm([-94.96, 129.91, 24.06])
 
 def box_inertia(mass: float, size_x: float, size_y: float, size_z: float) -> tuple[float, float, float]:
-    ixx = mass / 12.0 * (size_y**2 + size_z**2)
-    iyy = mass / 12.0 * (size_x**2 + size_z**2)
-    izz = mass / 12.0 * (size_x**2 + size_y**2)
+    ixx = mass / 12.0 * (pow(size_y,2) + pow(size_z,2))
+    iyy = mass / 12.0 * (pow(size_x,2) + pow(size_z,2))
+    izz = mass / 12.0 * (pow(size_x,2) + pow(size_y,2))
     return ixx, iyy, izz
 
+def coefs(mass: float, nrot: int, rpm_max: float, thrust2weight: float) -> tuple[float, float]:
+    g = 9.82
+    weight = mass * g
 
-def generate(mass: float) -> str:
+    # Maximum thrust required
+    thrust_max_total = weight * thrust2weight
+    thrust_max = thrust_max_total / nrot
 
+    # T = kf * RPM²
+    kf = thrust_max / rpm_max**2
 
-    ixx, iyy, izz = box_inertia(mass,SIZE_X, SIZE_Y, SIZE_Z)
+    # TODO: derive from propeller torque data
+    km = 9.9e-9
+    return kf, km
+
+def generate() -> str:
+    ixx, iyy, izz = box_inertia(MASS, SIZE_X, SIZE_Y, SIZE_Z)
+    kf, km = coefs(MASS, NROT, RPM_MAX, T2W)
 
     with open(os.path.join(SCRIPT_DIR, "starling2max.urdf.j2")) as f:
         template = jinja2.Template(f.read(), undefined=jinja2.StrictUndefined)
 
 
     urdf = template.render(
-        mass=mass,
+        kf=kf,
+        km=km,
+        mass=MASS,
         ixx=ixx,
         iyy=iyy,
         izz=izz,
@@ -57,4 +85,4 @@ def generate(mass: float) -> str:
     print("Wrote starling2max.urdf")
 
 if __name__ == "__main__":
-    generate(0.566)
+    generate()
