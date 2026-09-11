0d46e44f3991309feec3f525aefa9e5ad5b9d618
BjarkeHJ <bjarke_holm@hotmail.com>
2026-09-11 14:29:10 +0200

    starling max 2 urdf

---
+++ model/make_urdf.py
@@ -0,0 +1,49 @@
+
+import os
+import argparse
+import jinja2
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+MM_TO_M = 0.001
+
+# Measured Quantities
+SIZE_X = 217 * MM_TO_M
+SIZE_Y = 287 * MM_TO_M
+SIZE_Z = 129 * MM_TO_M
+
+def box_inertia(mass: float, size_x: float, size_y: float, size_z: float) -> tuple[float, float, float]:
+    ixx = mass / 12.0 * (size_y**2 + size_z**2)
+    iyy = mass / 12.0 * (size_x**2 + size_z**2)
+    izz = mass / 12.0 * (size_x**2 + size_y**2)
+    return ixx, iyy, izz
+
+
+def generate(mass: float) -> str:
+
+
+    ixx, iyy, izz = box_inertia(mass,SIZE_X, SIZE_Y, SIZE_Z)
+
+    with open(os.path.join(SCRIPT_DIR, "starling2max.urdf.j2")) as f:
+        template = jinja2.Template(f.read(), undefined=jinja2.StrictUndefined)
+
+
+    urdf = template.render(
+        mass=mass,
+        ixx=ixx,
+        iyy=iyy,
+        izz=izz,
+        size_x=SIZE_X,
+        size_y=SIZE_Y,
+        size_z=SIZE_Z,
+        scale_x=MM_TO_M,
+        scale_y=MM_TO_M,
+        scale_z=MM_TO_M
+    )
+
+    with open(os.path.join(SCRIPT_DIR, "starling2max.urdf"), "w") as f:
+        f.write(urdf)
+    print("Wrote starling2max.urdf")
+
+if __name__ == "__main__":
+    generate(0.566)
